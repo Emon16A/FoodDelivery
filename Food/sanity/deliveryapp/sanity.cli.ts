3639d2ba4514d803ebc27f6891e12ce514c4e1ba
Saiful Islam Emon <140347138+Emon16A@users.noreply.github.com>
2024-04-23 20:01:02 +0600

    comit

--- Food/sanity/deliveryapp/sanity.cli.ts
+++ Food/sanity/deliveryapp/sanity.cli.ts
@@ -1,8 +1,8 @@
-import {defineCliConfig} from 'sanity/cli'
+import { defineCliConfig } from 'sanity/cli'
 
 export default defineCliConfig({
   api: {
     projectId: 'uti3i56z',
-    dataset: 'production'
+    dataset: 'foodapp'
   }
 })
--- Food/sanity/sanity.config.ts
+++ Food/sanity/sanity.config.ts
@@ -1,14 +1,14 @@
-import {defineConfig} from 'sanity'
-import {deskTool} from 'sanity/desk'
-import {visionTool} from '@sanity/vision'
-import {schemaTypes} from './schemas'
+import { visionTool } from '@sanity/vision'
+import { defineConfig } from 'sanity'
+import { deskTool } from 'sanity/desk'
+import { schemaTypes } from './schemas'
 
 export default defineConfig({
   name: 'default',
   title: 'DeliveryApp',
 
   projectId: 'uti3i56z',
-  dataset: 'production',
+  dataset: 'foodapp',
 
   plugins: [deskTool(), visionTool()],
 
